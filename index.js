480d9c2c4c32758532ac4cd2056a16871ba66dfb
Stefan Vranic <stefkev@outlook.com>
2018-10-31 12:16:08 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict"
+// {
+//   user: <unique_id_of_sender>,
+//   channel: <unique_id_of_channel>,
+//   text: <text, payload or other value, if any>,
+//   type: <type of message or event>,
+//   raw_message: <original unmodified version of the message>
+// }
+
+var debug = require("debug")('line-middleware:index')
+var merge = require("deepmerge");
+
+
+module.exports = {
+  
+}
